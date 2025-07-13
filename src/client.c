3a88fe86488d253bb1533f0fc8e170454580d7ef
Marine Podevin <mapodevi@f2r4s22.clusters.42paris.fr>
2025-07-13 15:16:09 +0200

    test ta mere

--- src/client.c
+++ src/client.c
@@ -5,49 +5,94 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mapodevi <mapodevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/05 15:32:19 by mapodevi          #+#    #+#             */
-/*   Updated: 2025/07/10 11:23:51 by mapodevi         ###   ########.fr       */
+/*   Created: 2025/07/13 15:07:52 by mapodevi          #+#    #+#             */
+/*   Updated: 2025/07/13 15:09:44 by mapodevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-void	ft_send_bits(int pid, char i)
+int	g_bit = 0;
+
+void	ft_sigprint(int sig)
+{
+	if (sig == SIGUSR1)
+		g_bit = 1;
+	else if (sig == SIGUSR2)
+	{
+		ft_printf("Message received !\n");
+		exit(EXIT_SUCCESS);
+	}
+}
+
+static void	ft_send_bits(int pid, char i)
 {
 	int	bit;
 
 	bit = 0;
 	while (bit < 8)
 	{
-		if ((i & (1 << bit)) != 0)
+		g_bit = 0;
+		if ((i >> bit) & 1)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(200);
 		bit++;
+		while (g_bit == 0)
+			pause();
 	}
+}
+
+static void	ft_send_len(int pid, int len)
+{
+	int		i;
+	char	byte;
+
+	i = 0;
+	while (i < 4)
+	{
+		byte = (len >> (i * 8)) & 0xFF;
+		ft_send_bits(pid, byte);
+		i++;
+	}
+}
+
+static void	ft_send_str(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_send_bits(pid, str[i]);
+		i++;
+	}
+	ft_send_bits(pid, '\n');
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
-	int	i;
+	int		pid;
+	int		i;
+	int		len;
+	char	*msg;
 
 	i = 0;
+	len = 0;
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
-		while (argv[2][i] != '\0')
-		{
-			ft_send_bits(pid, argv[2][i]);
-			i++;
-		}
-		ft_send_bits(pid, '\n');
+		msg = argv[2];
+		len = ft_strlen(msg);
+		signal(SIGUSR1, &ft_sigprint);
+		signal(SIGUSR2, &ft_sigprint);
 	}
 	else
 	{
 		ft_printf("Error\n");
 		return (1);
 	}
+	ft_send_len(pid, len);
+	ft_send_str(pid, argv[2]);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -5,32 +5,138 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mapodevi <mapodevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/05 15:32:28 by mapodevi          #+#    #+#             */
-/*   Updated: 2025/07/10 10:43:49 by mapodevi         ###   ########.fr       */
+/*   Created: 2025/07/13 15:08:23 by mapodevi          #+#    #+#             */
+/*   Updated: 2025/07/13 15:15:57 by mapodevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
-
-void	ft_handler(int signal)
+/* void	ft_handler(int signal, siginfo_t *info, void *context)
 {
-	static int	bit;
-	static int	i;
+	(void)context;
+
+	static int		bit = 0;
+	static int		byte = 0;
+	static int		state = 0;
+	static int		len = 0;
+	static char		*msg = NULL;
+	static int		received_chars = 0;
 
 	if (signal == SIGUSR1)
-		i |= (1 << bit);
+		byte |= (1 << bit);
+	else if (signal == SIGUSR2)
+		byte &= ~(1 << bit);
 	bit++;
+
 	if (bit == 8)
 	{
-		ft_printf("%c", i);
+		if (state == 0)
+		{
+			len |= (byte << (received_chars * 8));
+			received_chars++;
+			if (received_chars == 4)
+			{
+				if (len <= 0 || len > 1000000)
+				{
+					ft_printf("Invalid len: %d\n", len);
+					exit(1);
+				}
+				msg = malloc(len + 1);
+				if (!msg)
+				{
+					ft_printf("Malloc failed\n");
+					exit(1);
+				}
+				msg[len] = '\0';
+				state = 1;
+				received_chars = 0;
+			}
+		}
+		else if (state == 1)
+		{
+			msg[received_chars++] = byte;
+			if (received_chars == len)
+			{
+				// ft_printf("%s\n", msg);
+				write(1, msg, len);
+				kill(info->si_pid, SIGUSR2);
+				free(msg);
+				msg = NULL;
+				len = 0;
+				state = 0;
+				received_chars = 0;
+			}
+		}
 		bit = 0;
-		i = 0;
+		byte = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
+} */
+static void	ft_init_msg(int *len, char **msg)
+{
+	if (*len <= 0 || *len > 1000000)
+	{
+		ft_printf("Invalid len: %d\n", *len);
+		exit(1);
+	}
+	*msg = malloc(*len + 1);
+	if (!*msg)
+	{
+		ft_printf("Malloc failed\n");
+		exit(1);
+	}
+	(*msg)[*len] = '\0';
+}
+
+static void	ft_handle_len(int *len, int *byte, int *received, int *state, char **msg)
+{
+	*len |= (*byte << (*received * 8));
+	if (++(*received) == 4)
+	{
+		ft_init_msg(len, msg);
+		*state = 1;
+		*received = 0;
 	}
 }
+
+static void	ft_handle_msg(int *len, int *received, int *state, char **msg, int byte, pid_t pid)
+{
+	(*msg)[(*received)++] = byte;
+	if (*received == *len)
+	{
+		write(1, *msg, *len);
+		kill(pid, SIGUSR2);
+		free(*msg);
+		*msg = NULL;
+		*len = 0;
+		*state = 0;
+		*received = 0;
+	}
+}
+
+void	ft_handler(int signal, siginfo_t *info, void *context)
+{
+	(void)context;
+	static int bit, byte, state, len, received;
+	static char *msg;
+
+	if (signal == SIGUSR1) byte |= (1 << bit);
+	else if (signal == SIGUSR2) byte &= ~(1 << bit);
+	if (++bit == 8)
+	{
+		if (state == 0) ft_handle_len(&len, &byte, &received, &state, &msg);
+		else ft_handle_msg(&len, &received, &state, &msg, byte, info->si_pid);
+		bit = 0;
+		byte = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
+}
+
 
 int	main(int argc, char **argv)
 {
 	int	pid;
+	struct sigaction	sig;
 
 	(void)argv;
 	if (argc != 1)
@@ -41,10 +147,13 @@ int	main(int argc, char **argv)
 	pid = getpid();
 	ft_printf("PID : %d\n", pid);
 	ft_printf("Waiting for a message...\n");
-	while (argc == 1)
+	sig.sa_sigaction = ft_handler;
+	sigemptyset(&sig.sa_mask);
+	sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	while (1)
 	{
-		signal(SIGUSR1, ft_handler);
-		signal(SIGUSR2, ft_handler);
 		pause ();
 	}
 	return (0);
